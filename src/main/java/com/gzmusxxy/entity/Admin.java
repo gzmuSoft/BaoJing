53f40354d9f5fda82f8e3175658b6fc790b63c3a
YXF <1025451608@qq.com>
2019-08-12 11:25:51 +0800

    ：：

--- src/main/java/com/gzmusxxy/entity/Admin.java
+++ src/main/java/com/gzmusxxy/entity/Admin.java
@@ -1,5 +1,8 @@
 package com.gzmusxxy.entity;
 
+import lombok.Data;
+
+@Data
 public class Admin {
     private Integer id;
 
@@ -11,43 +14,5 @@ public class Admin {
 
     private Integer role;
 
-    public Integer getId() {
-        return id;
-    }
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public Integer getRole() {
-        return role;
-    }
-
-    public void setRole(Integer role) {
-        this.role = role;
-    }
 }
--- src/main/java/com/gzmusxxy/entity/BxProject.java
+++ src/main/java/com/gzmusxxy/entity/BxProject.java
@@ -1,6 +1,7 @@
 package com.gzmusxxy.entity;
 
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 @Data
@@ -9,8 +10,12 @@ public class BxProject {
 
     private String name;
 
-    private Date startTime;
+    private String claimsTemplateName;
 
+    private String claimsTemplate;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date startTime;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date endTime;
 
     private Float cost;

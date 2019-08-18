56891355cc800e5f7367ea84e39b879d765f5f08
RLDS <1782832653@qq.com>
2019-08-18 16:25:33 +0800

    :sparkles:提交新的表的mapper文件和实体类

---
+++ src/main/java/com/gzmusxxy/entity/Bulletin.java
@@ -0,0 +1,65 @@
+package com.gzmusxxy.entity;
+
+import java.util.Date;
+
+public class Bulletin {
+    private Integer id;
+
+    private String title;
+
+    private String content;
+
+    private Integer sourceId;
+
+    private Integer createAdminId;
+
+    private Date updateTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Integer getSourceId() {
+        return sourceId;
+    }
+
+    public void setSourceId(Integer sourceId) {
+        this.sourceId = sourceId;
+    }
+
+    public Integer getCreateAdminId() {
+        return createAdminId;
+    }
+
+    public void setCreateAdminId(Integer createAdminId) {
+        this.createAdminId = createAdminId;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
---
+++ src/main/java/com/gzmusxxy/mapper/BulletinMapper.java
@@ -0,0 +1,16 @@
+package com.gzmusxxy.mapper;
+
+import com.gzmusxxy.entity.Bulletin;
+import java.util.List;
+
+public interface BulletinMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Bulletin record);
+
+    Bulletin selectByPrimaryKey(Integer id);
+
+    List<Bulletin> selectAll();
+
+    int updateByPrimaryKey(Bulletin record);
+}

d600f260028a6a94ee7c5cc0d9f05a4e26a3af8e
qfmx <1125438556@qq.com>
2019-09-15 00:48:50 +0800

    添加报京文苑模块功能

--- src/main/java/com/gzmusxxy/controller/ColorfulController.java
+++ src/main/java/com/gzmusxxy/controller/ColorfulController.java
@@ -2,8 +2,11 @@ package com.gzmusxxy.controller;
 
 import com.github.pagehelper.PageInfo;
 import com.gzmusxxy.common.JsonResult;
+import com.gzmusxxy.entity.QueArticle;
 import com.gzmusxxy.entity.QueImages;
+import com.gzmusxxy.service.QueArticleService;
 import com.gzmusxxy.service.QueImagesService;
+import com.gzmusxxy.service.QueVideosService;
 import com.gzmusxxy.util.FileUtil;
 import com.gzmusxxy.util.PageUtil;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +16,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * 多彩报京
  */
@@ -21,7 +29,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class ColorfulController {
     @Autowired
     private QueImagesService queImagesService;
-
+    @Autowired
+    private QueArticleService queArticleService;
+    @Autowired
+    private QueVideosService queVideosService;
     /**
      * 图片展示
      * @return
@@ -31,7 +42,7 @@ public class ColorfulController {
         PageInfo<QueImages> queImagesPageInfo = queImagesService.selectAll(pageNumber > 0 ? pageNumber : 1);
         model.addAttribute("pageInfo",queImagesPageInfo);
         model.addAttribute("pages", PageUtil.getPage(queImagesPageInfo.getPages(), pageNumber));
-        return "colorful/images.html";
+        return "colorful/images";
     }
     @RequestMapping(value = "/deletePhotosById")
     @ResponseBody
@@ -52,6 +63,124 @@ public class ColorfulController {
         }
         jsonResult.setCode(0);
         jsonResult.setResult("删除失败！");
+        return jsonResult;
+    }
+
+    /**
+     * 获取所有文章
+     * @return List类型的实例
+     */
+    @RequestMapping(value = "/getAllArticle")
+    @ResponseBody
+    public List<QueArticle> getAllArticle(){
+        List<QueArticle> list = this.queArticleService.selectAll();
+        try{
+            if(list != null && list.size() > 0){
+                Map<String,Object> map = new HashMap<>();
+                map.put("articleList",list);
+                return list;
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return list;
+    }
+    /**
+     * 文章分页
+     * @param pageNumber 页码
+     * @param model 模型
+     * @return 返回到文章页
+     */
+    @RequestMapping(value = "/article")
+    public String selectByArticlePage(@RequestParam("pageNumber") Integer pageNumber, Model model){
+        PageInfo<QueArticle> pageInfo = this.queArticleService.selectByPage(pageNumber>0?pageNumber:1);
+        model.addAttribute("pageInfo",pageInfo);
+        model.addAttribute("pages", PageUtil.getPage(pageInfo.getPages(),pageNumber));
+        return "colorful/article";
+    }
+    /**
+     * 根据文章编号删除文章
+     * @param id  文章编号
+     * @return JsonResult
+     */
+    @RequestMapping(value = "/deleteArticle")
+    @ResponseBody
+    public JsonResult deleteArticle(@RequestParam("id") Integer id){
+        JsonResult jsonResult = new JsonResult();
+        int i = this.queArticleService.deleteByPrimaryKey(id);
+        if(i > 0){
+            //删除成功
+            jsonResult.setCode(1);
+            jsonResult.setResult("文章删除成功");
+            return jsonResult;
+        }
+        // 删除文章失败
+        jsonResult.setCode(0);
+        jsonResult.setResult("文章删除失败");
+        return jsonResult;
+    }
+
+    /**
+     * 添加文章
+     * @param queArticle 文章信息
+     * @return JsonResult
+     */
+    @RequestMapping(value = "/addArticle")
+    @ResponseBody
+    public JsonResult addArticle(QueArticle queArticle){
+        JsonResult jsonResult = new JsonResult();
+        queArticle.setCreateTime(new Date());
+        int insert = this.queArticleService.insert(queArticle);
+        if(insert > 0){
+            jsonResult.setCode(1);
+            jsonResult.setResult("添加成功");
+            return jsonResult;
+        }
+        jsonResult.setCode(0);
+        jsonResult.setResult("添加失败");
+        return jsonResult;
+    }
+    /**
+     * 根据文章id获取文章
+     * @param id 文章id
+     * @return 文章完整信息
+     */
+    @RequestMapping(value = "/getArticleById")
+    @ResponseBody
+    public QueArticle getArticleById(@RequestParam("id") Integer id){
+        return this.queArticleService.selectByPrimaryKey(id);
+    }
+    /**
+     * 获得文章信息编辑文章页面：用于填充编辑框
+     * @param id 编号
+     * @param model model
+     * @return 返回到编辑页面
+     */
+    @RequestMapping(value = "/getArticleMsg")
+    public String  getArticleMsg(@RequestParam("id") Integer id,Model model){
+        QueArticle article = this.queArticleService.selectByPrimaryKey(id);
+        model.addAttribute("article",article);
+        return "colorful/editArticle";
+    }
+    /**
+     * 更新文章信息
+     * @param queArticle 更新的文章信息
+     * @return 返回JsonResult类型的数据
+     */
+    @RequestMapping(value = "/updateArticle")
+    @ResponseBody
+    public JsonResult updateArticle(QueArticle queArticle){
+        JsonResult jsonResult = new JsonResult();
+        queArticle.setCreateTime(new Date());
+        int i = this.queArticleService.updateByPrimaryKey(queArticle);
+        if (i > 0){
+            jsonResult.setCode(1);
+            jsonResult.setResult("更新数据成功");
+            return jsonResult;
+        }
+        // 失败返回0
+        jsonResult.setCode(0);
+        jsonResult.setResult("更新数据失败");
         return jsonResult;
     }
 }
--- src/main/java/com/gzmusxxy/service/impl/QueArticleServiceImpl.java
+++ src/main/java/com/gzmusxxy/service/impl/QueArticleServiceImpl.java
@@ -1,10 +1,14 @@
 package com.gzmusxxy.service.impl;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.gzmusxxy.entity.QueArticle;
 import com.gzmusxxy.mapper.QueArticleMapper;
 import com.gzmusxxy.service.QueArticleService;
+import com.gzmusxxy.util.PageUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -17,12 +21,12 @@ import java.util.List;
 public class QueArticleServiceImpl implements QueArticleService {
     @Autowired
     private QueArticleMapper queArticleMapper;
-
+    @Transactional
     @Override
     public int deleteByPrimaryKey(Integer id) {
         return queArticleMapper.deleteByPrimaryKey(id);
     }
-
+    @Transactional
     @Override
     public int insert(QueArticle record) {
         return queArticleMapper.insert(record);
@@ -37,9 +41,17 @@ public class QueArticleServiceImpl implements QueArticleService {
     public List<QueArticle> selectAll() {
         return queArticleMapper.selectAll();
     }
-
+    @Transactional
     @Override
     public int updateByPrimaryKey(QueArticle record) {
         return queArticleMapper.updateByPrimaryKey(record);
+    }
+    @Transactional
+    @Override
+    public PageInfo<QueArticle> selectByPage(Integer pageNumber) {
+        //PageHelper插件的分页信息：默认8条数据
+        PageHelper.startPage(pageNumber, PageUtil.PAGE_ROW_COUNT);
+        List<QueArticle> list = this.queArticleMapper.selectAll();
+        return new PageInfo<>(list);
     }
 }

1a539f42465b6abe199300d33a8e894ebeb409ab
RLDS <1782832653@qq.com>
2019-10-02 12:33:49 +0800

    添加Mybatis映射文件

---
+++ src/main/java/com/gzmusxxy/mapper/ZfPhotoMapper.java
@@ -0,0 +1,16 @@
+package com.gzmusxxy.mapper;
+
+import com.gzmusxxy.entity.ZfPhoto;
+import java.util.List;
+
+public interface ZfPhotoMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(ZfPhoto record);
+
+    ZfPhoto selectByPrimaryKey(Integer id);
+
+    List<ZfPhoto> selectAll();
+
+    int updateByPrimaryKey(ZfPhoto record);
+}
